b5f8c58af7baed142bd70bb9bd88d9e05e8bf512
unknown <alistair.mcnair@gmail.com>
2020-10-12 20:13:09 +0100

    Initial commit

---
+++ Public/New-nestedESXi.ps1
@@ -0,0 +1,263 @@
+﻿function New-nestedESXi {
+    <#
+    .SYNOPSIS
+        Deploy a nested ESXi VM from template.
+
+    .DESCRIPTION
+        Deploy a nested ESXi VM from template.
+        Configures 2 standard NICs (1500) MTU and 2 vSAN NICs (9000) MTU
+        Configures cores, RAM and hard drives from paramters.
+        Configures hard drives as SSD.
+
+    .PARAMETER ovaIndex
+        The index number of the DML item to deploy from.
+
+    .PARAMETER dataStore
+        The name of the target datastore to deploy to.
+
+    .PARAMETER vmName
+        The name of the VM being deployed.
+
+    .PARAMETER esxRAM
+        The amount of RAM to configure on the VM in GB.
+
+    .PARAMETER esxCores
+        The amount of core to configure on the VM.
+
+    .PARAMETER esxHD01
+        Optional. The capacity of additional hard drive 1 in GB.
+
+    .PARAMETER esxHD02
+        Optional. The capacity of additional hard drive 2 in GB.
+
+    .PARAMETER esxHD03
+        Optional. The capacity of additional hard drive 3 in GB.
+
+    .PARAMETER esxNestedNet01
+        The network to attach the first pair of NICs to.
+
+    .PARAMETER esxNestedNet02
+        The network to attach the second pair of NICs to.
+
+    .PARAMETER reserveMem
+        Optional. Reserve all of the configured VM RAM. Avoid swap file creation.
+
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        None.
+
+    .EXAMPLE
+        New-nestedESXi -ovfPath C:\DML\esxi_67u2\esxi_67u2.ovf -dataStore datastore01 -vmName nestedVM01
+        -vmHost host01.local -esxRAM 16 -esxCores 4 -esxHD01 20 -esxHD02 30 -esxHD03 40
+        -esxNestedNet01 pg01Pod02 -esxNestedNet02 pg02Pod02 -reserveMem $true -Verbose
+
+        Deploy a nested ESXi host using the specified paramters.
+
+    .LINK
+
+    .NOTES
+        01           Alistair McNair          Initial version.
+
+    #>
+
+    [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact="Low")]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$ovfPath,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$dataStore,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string]$vmName,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$vmHost,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxRAM,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxCores,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxHD01,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxHD02,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxHD03,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxNestedNet01,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [string]$esxNestedNet02,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [bool]$reserveMem
+    )
+
+
+    begin {
+
+        Write-Verbose ("Function start.")
+
+    } # begin
+
+
+    process {
+
+        Write-Verbose ("Processing nested ESXI host on VM " + $vmName)
+
+        ## Should process
+        if ($PSCmdlet.ShouldProcess($vmName)) {
+
+            Write-Verbose ("Deploying OVA: " + $ovfPath)
+
+            ## Import the OVA
+            try {
+                Import-VApp -Source $ovfPath -Name $vmName -VMHost $vmHost -DiskStorageFormat Thin -Datastore $dataStore -Verbose:$false -ErrorAction Stop | Out-Null
+                Write-Verbose ("Deployed OVA.")
+            } # try
+            catch {
+                throw ("Failed to deploy OVA " + $ovfPath + " , the CMDlet returned " + $_.exception.message)
+            } # catch
+
+
+            ## Get the VM object
+            try {
+                $vm = Get-VM -Name $vmName -Verbose:$false -ErrorAction Stop
+                Write-Verbose ("Got VM object.")
+            } # try
+            catch {
+                throw ("Failed to get VM object, CMDlet returned " + $_.exception.message)
+            } # catch
+
+
+            Write-Verbose ("Stripping existing NICs from VM.")
+
+
+            ## Remove all existing net adapters
+            try {
+                $vm | Get-NetworkAdapter -Verbose:$false | Remove-NetworkAdapter -Confirm:$false -Verbose:$false -ErrorAction Stop
+                Write-Verbose ("Removed existing NICs.")
+            } # try
+            catch {
+                throw ("Failed to remove existing net adapters, CMDlet returned " + $_.exception.message)
+            } # catch
+
+
+            ## Add 2 new non-vsan net adapters (1500 MTU)
+            try {
+                for ($i = 1; $i -le 2; $i++) {
+
+                    Write-Verbose ("Adding non-vSAN adapter " + $i + " of 2 to network " + $esxNestedNet01)
+                    New-NetworkAdapter -VM $VM -NetworkName $esxNestedNet01 -StartConnected -Verbose:$false -ErrorAction Stop | Out-Null
+
+                } # for
+
+            } # try
+            catch {
+                throw ("Failed to add net adapter, the CMDlet returned " + $_.exception.message)
+            } # catch
+
+
+            ## Add 2 new vsan net adapters (9000 MTU)
+            try {
+
+                for ($i = 1; $i -le 2; $i++) {
+
+                    Write-Verbose ("Adding vSAN adapter " + $i + " of 2 to network " + $esxNestedNet02)
+                    New-NetworkAdapter -VM $VM -NetworkName $esxNestedNet02 -StartConnected -Verbose:$false -ErrorAction Stop | Out-Null
+
+                } # for
+
+            } # try
+            catch {
+                throw ("Failed to add net adapter, the CMDlet returned " + $_.exception.message)
+            } # catch
+
+
+            ## Configure CPU and RAM
+            try {
+                $config = Set-VM -VM $VM -MemoryGB $esxRAM -CoresPerSocket $esxCores -NumCpu $esxCores -Confirm:$false -Verbose:$false -ErrorAction Stop
+                Write-Verbose ("Host CPU and RAM set to " + $esxRAM + "GB and " + $esxCores + " cores.")
+            } # try
+            catch {
+                throw ("Failed to configure host RAM and CPU, CMDlet returned " + $_.exception.message)
+            } # catch
+
+            $i = 0
+
+            foreach ($esxHD in @($esxHD01,$esxHD02,$esxHD03)) {
+
+                $i++
+
+                ## Add hard drives, if specified
+                if ($esxHD) {
+
+                    try {
+                        $config = $VM | New-HardDisk -CapacityGB $esxHD -Controller "SCSI Controller 0" -StorageFormat Thin -Datastore $dataStore -Verbose:$false -ErrorAction Stop
+                        Write-Verbose ("Hard disk " + $i + " specified of capacity " + $esxHD + "GB")
+                    } # try
+                    catch {
+                        throw ("Failed to add hard disk, the CMDlet returned " + $_.exception.message)
+                    } # catch
+
+                    ## Tag this disk as SSD
+                    try {
+                        New-AdvancedSetting -Entity $VM -Name ("scsi0:" + $config.ExtensionData.UnitNumber + ".virtualSSD") -Value 1 -Confirm:$false -Verbose:$false -ErrorAction Stop | Out-Null
+                        Write-Verbose ("Configured SSD emulation for device " + $config.ExtensionData.UnitNumber)
+                    } # try
+                    catch {
+                        throw ("Failed to tag disk " + $config.ExtensionData.UnitNumber + " as SSD, the CMDlet returned " + $_.exception.message)
+                    } # catch
+
+                    Write-Verbose ("Finished configuring hard disk " + $i)
+
+                } # if
+                else {
+                    Write-Verbose ("Hard disk " + $i + " was not specified.")
+                } # else
+
+            } # foeach
+
+
+            ## If specified, set 100% memory reservation on this VM to avoid swap file creation
+            if ($reserveMem) {
+                Write-Verbose ("100% memory reservation has been specified for this VM.")
+
+                try {
+                    $guestConfig = New-Object VMware.Vim.VirtualMachineConfigSpec
+                    $guestConfig.memoryReservationLockedToMax = $True
+                    $config = $VM.ExtensionData.ReconfigVM_task($guestConfig)
+                    Write-Verbose ("Memory reservation has been set.")
+                } # try
+                catch {
+                    throw ("Failed to set memory reservation, the CMDlet returned " + $_.exception.message)
+                } # catch
+
+            } # if
+            else {
+                Write-Verbose ("Memory reservation has not been specified. VM swap files will be created.")
+            } # else
+
+
+            ## Power on nested host
+            try {
+                Write-Verbose ("Waiting for nested host to boot.")
+                $config = Start-VM -VM $VM -Verbose:$false -ErrorAction Stop  | Wait-Tools -Verbose:$false
+                Write-Verbose ("Host booted.")
+            } # try
+            catch {
+                throw ("Failed to power on nested host, the CMDlet returned " + $_.exception.message)
+            } # catch
+
+
+            ## Return VM object of nested host
+            return $vm
+
+        } # if
+
+    } # process
+
+    End {
+        Write-Verbose ("Function complete.")
+
+    } # end
+
+} # function
---
+++ Public/Set-nestedESXi.ps1
@@ -0,0 +1,207 @@
+﻿function Set-nestedESXi {
+    <#
+    .SYNOPSIS
+        Configure a nested ESXi instance.
+
+    .DESCRIPTION
+        Configure a nested ESXi instance.
+            * Add and remove vmk0 to regenerate MAC address
+            * Configure management IP and subnet
+            * Configure a DNS server
+            * Set a hostname
+
+        This is performed via the Guest Management API
+
+    .PARAMETER vmName
+        VM name of nested ESXi to configure.
+
+    .PARAMETER mgmtIp
+        The management IP to configure.
+
+    .PARAMETER mgmtSubnet
+        The management subnet to configure.
+
+    .PARAMETER mgmtDNS
+        The DNS address to configure.
+
+    .PARAMETER hostName
+        The hostname to configure.
+
+    .INPUTS
+        None.
+
+    .OUTPUTS
+        None.
+
+    .EXAMPLE
+        Set-nestedESXi -vmName pod01esx01 -mgmtIp 10.10.1.1 -mgmtSubnet 255.255.255.0 -mgmtDNS 10.10.1.20 -hostName podesx01.pod01.local -Verbose
+
+        Configure the nested host in VM pod01esx01 with the IP and hostname.
+
+    .LINK
+
+    .NOTES
+        01           Alistair McNair          Initial version.
+
+    #>
+
+    [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact="Low")]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]$vmName,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]$mgmtIp,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]$mgmtSubnet,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]$mgmtDns,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [string]$hostName
+    )
+
+
+    begin {
+
+        Write-Verbose ("Function start.")
+
+    } # begin
+
+    process {
+
+        Write-Verbose ("Porcessing nested system " + $vmName)
+
+        ## Should process
+        if ($PSCmdlet.ShouldProcess($vmName)) {
+
+            ## Get the VM object
+            try {
+                $vm = Get-VM -Name $vmName -ErrorAction Stop -Verbose:$false
+                Write-Verbose ("Got VM object.")
+            } # try
+            catch {
+                Write-Debug ("Failed to get VM object.")
+                throw ("Failed to get VM object. " + $_.exception.message)
+            } # catch
+
+
+            ## Prepare Guest Operation API for nested ESXi
+            $guestOpMgr = Get-View $defaultVIServer.ExtensionData.Content.GuestOperationsManager -Verbose:$false
+            $procMgr = Get-View $guestOpMgr.processManager -Verbose:$False
+
+
+            ## Create Auth Session Object (using default appliance credentials)
+            $auth = New-Object VMware.Vim.NamePasswordAuthentication
+            $auth.username = "root"
+            $auth.password = "VMware1!"
+            $auth.InteractiveSession = $false
+
+
+            ## Program Spec
+            $progSpec = New-Object VMware.Vim.GuestProgramSpec
+
+
+            # Full path to the command to run inside the guest
+            $progSpec.programPath = "/bin/python"
+            $progSpec.workingDirectory = "/tmp"
+
+
+            ## Remove VMK0 and re-add it. The default VMK0 inherits the MAC address from the physical adapter, which causes issues with nested networking
+            ## Removing and re-adding this adapter forces a new MAC address which is unique
+            $progSpec.arguments = "++group=host/vim/tmp /bin/esxcli.py network ip interface remove --interface-name=vmk0"
+
+            Write-Verbose ("Removing and adding VMK0 to generate new MAC address.")
+
+            try {
+                $procMgr.StartProgramInGuest($vm.ExtensionData.MoRef,$auth,$progSpec) | Out-Null
+                Write-Verbose ("vmk0 removed.")
+            } # try
+            catch {
+                Write-Debug ("Failed to remove vmk0")
+                throw ("Failed to remove vmk0. " + $_.exception.message)
+            } # catch
+
+            Start-Sleep 5
+
+            ## Create a new VMK0
+            $progSpec.arguments = "++group=host/vim/tmp /bin/esxcli.py network ip interface add --interface-name=vmk0 --portgroup-name=`"Management Network`""
+
+            try {
+                $procMgr.StartProgramInGuest($vm.ExtensionData.MoRef,$auth,$progSpec) | Out-Null
+                Write-Verbose ("vmk0 created.")
+            } # try
+            catch {
+                Write-Debug ("Failed to create vmk0.")
+                throw ("Failed to create vmk0. " + $_.exception.message)
+            } # catch
+
+            Start-Sleep 5
+
+
+            Write-Verbose ("Configuring management IP and subnet.")
+
+
+            ## Prepare command to set IP
+            $progSpec.arguments = "++group=host/vim/tmp /bin/esxcli.py network ip interface ipv4 set -i vmk0 -I " + $mgmtIp + " -N " + $mgmtSubnet + " -t static"
+
+
+            ## Execute nested host esxcli command
+            try {
+                $procMgr.StartProgramInGuest($VM.ExtensionData.MoRef,$auth,$progSpec) | Out-Null
+                Write-Verbose ("Management IP and subnet configured to " + $mgmtIp + " / " + $mgmtSubnet)
+            } # try
+            catch {
+                Write-Debug ("Failed to set management IP.")
+                throw ("Failed to configure managament IP. " + $_.exception.message)
+            } # catch
+
+
+            ## Prepare command to set DNS
+            $progSpec.arguments = ("/bin/esxcli.py network ip dns server add -s " + $mgmtDNS)
+
+
+            Write-Verbose ("Configuring DNS address.")
+
+
+            ## Execute nested host esxcli command
+            try {
+                $procMgr.StartProgramInGuest($vm.ExtensionData.MoRef,$auth,$progSpec) | Out-Null
+                Write-Verbose ("DNS address was set to "  +$mgmtDns)
+            } # try
+            catch {
+                Write-Debug ("Failed to configure DNS.")
+                throw ("Failed to configure DNS " + $_.exception.message)
+            } # catch
+
+
+            Write-Verbose ("Configuring hostname.")
+
+
+            ## Prepare command to set hostname
+            $progSpec.arguments = ("/bin/esxcli.py system hostname set --host=" + $hostName)
+
+
+            ## Execute nested host esxcli command
+            try {
+                $procMgr.StartProgramInGuest($VM.ExtensionData.MoRef,$auth,$progSpec) | Out-Null
+                Write-Verbose ("Hostname set to " + $hostName)
+            } # try
+            catch {
+                Write-Debug ("Failed to set hostname.")
+                throw ("Failed to set hostname. " + $_.exception.message)
+            } # catch
+
+
+        } # if
+
+        Write-Verbose ("Completed processing host.")
+
+    } # process
+
+    end {
+
+        Write-Verbose ("Function complete..")
+    } # end
+
+
+} # function
